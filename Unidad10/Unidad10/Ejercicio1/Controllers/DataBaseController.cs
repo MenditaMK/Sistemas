135f6a13b8e64ca914fdbab68b57df486e284b38
MenditaMK <72132123+MenditaMK@users.noreply.github.com>
2020-11-12 13:46:33 +0100

    d

---
+++ Unidad10/Unidad10/Ejercicio1/Controllers/DataBaseController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace Ejercicio1.Controllers
+{
+    public class DataBaseController : Controller
+    {
+        // GET: DataBase
+        public ActionResult Index()
+        {
+     
+            return View();
+        }
+
+        [HttpPost, ActionName("Index")]
+        public ActionResult IndexPost() {
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+                connection.ConnectionString = "server = jaquintero.database.windows.net; database = jaquintero; uid = prueba; pwd = Mitesoro1.";
+
+                connection.Open();
+            }
+            catch (SqlException exception) { 
+                
+            }
+            return View();
+        }
+    }
+}
