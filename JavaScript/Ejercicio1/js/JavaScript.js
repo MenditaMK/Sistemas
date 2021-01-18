beb14eac9f6ebb55b30e2c9e5b89de0a42e20399
MenditaMK <72132123+MenditaMK@users.noreply.github.com>
2021-01-18 14:34:44 +0100

    f

---
+++ JavaScript/Ejercicio1/js/JavaScript.js
@@ -0,0 +1,16 @@
+﻿window.onload = inicializarEventos;
+
+function inicializarEventos() {
+    document.getElementById("btnSaludar").addEventListener("click", saludar, false);
+}
+
+function saludar() {
+    alert("HELLO FUUUUCKING WOOOOOOORLLLLD");
+}
+
+class Persona{
+    constructor(nombre, apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+}
