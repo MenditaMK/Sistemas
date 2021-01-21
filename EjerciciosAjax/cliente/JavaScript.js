30ab222fb5102955f11898a52d86d729fd3b743b
MenditaMK <72132123+MenditaMK@users.noreply.github.com>
2021-01-21 18:42:35 +0100

    v

---
+++ EjerciciosAjax/cliente/JavaScript.js
@@ -0,0 +1,21 @@
+﻿window.onload = inicializa;
+
+function inicializa() {
+    document.getElementById("saluda").addEventListener("click", saludar, false);
+}
+
+function saludar() {
+    var llamada = new XMLHttpRequest();
+    llamada.open("GET", "http://localhost:56626/servidor/Archivo.html");
+
+    llamada.onreadystatechange = function () {
+
+        if (llamada.readyState < 4) {
+
+        } else if (llamada.readyState == 4 && llamada.status == 200) {
+            var saludo = llamada.responseText;
+            document.getElementById("respuesta").innerHTML = saludo;
+        }
+    };
+    llamada.send();
+}
