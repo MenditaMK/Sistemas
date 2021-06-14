972951b08511a90622f96e75cc7ff26728f5955c
jaquinterogomez <72132123+jaquinterogomez@users.noreply.github.com>
2021-06-14 11:13:25 +0200

    t

--- Unidad10CRUD/CRUDPersonas/CRUDPersonas_Entidades/clsListadoArtistasEvento.cs
+++ Unidad10CRUD/CRUDPersonas/CRUDPersonas_Entidades/clsListadoArtistasEvento.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace API.Models
+namespace CRUDPersonas_Entidades
 {
     public class clsListadoArtistasEvento
     {
